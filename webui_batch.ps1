7177252c21a7c4dfb11af9fe8ebb7078bdcafa95
sdbds <865105819@qq.com>
2023-06-14 00:55:53 +0800

    update for main repo

--- webui_batch.ps1
+++
@@ -1,8 +0,0 @@
-Set-Location $PSScriptRoot
-.\venv\Scripts\activate
-Set-Location .\audiocraft
-
-$Env:HF_HOME = "../huggingface"
-$Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
-
-python.exe app_batched.py $args
